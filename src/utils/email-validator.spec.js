e65ae0aa9d61d9315a27079d74aad4e54e6a3855
Ricardo Borges <ricardo.borges@somosphi.com>
2020-07-20 21:00:42 -0300

    add email-validator

---
+++ src/utils/email-validator.spec.js
@@ -0,0 +1,28 @@
+const validator = require('validator')
+const EmailValidator = require('./email-validator')
+
+const makeSut = () => {
+  return new EmailValidator()
+}
+
+describe('Email Validator', () => {
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('email@email.com')
+    expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('email@email.com')
+    expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    const email = 'email@email.com'
+    sut.isValid(email)
+    expect(validator.email).toBe(email)
+  })
+})

ed45192842b4b24790a6928f6c50bdc380d48dda
Ricardo Borges <ricardo.borges@somosphi.com>
2020-07-25 17:44:14 -0300

    add token-generator

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken')
+const TokenGenerator = require('./token-generator')
+const MissingParamError = require('../errors/missing-param-error')
+
+const makeSut = () => {
+  return new TokenGenerator('secret')
+}
+
+describe('TokenGenerator', () => {
+  test('Should return null if jwt returns null', async () => {
+    const sut = makeSut()
+    jwt.token = null
+    const token = await sut.generate('id')
+    expect(token).toBe(null)
+  })
+
+  test('Should return a token if jwt returns token', async () => {
+    const sut = makeSut()
+    const token = await sut.generate('id')
+    expect(token).toBe(jwt.token)
+  })
+
+  test('Should call jwt with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('id')
+    expect(jwt.id).toBe('id')
+    expect(jwt.secret).toBe(sut.secret)
+  })
+
+  test('Should throw if no secret is provided', async () => {
+    const sut = new TokenGenerator()
+    const promise = sut.generate('id')
+    expect(promise).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('Should throw if no id is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
+  })
+})

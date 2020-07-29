6fe08655326da602202d3fd45ea4cb0e02182def
Ricardo Borges <ricardo.borges@somosphi.com>
2020-07-29 20:23:18 -0300

    fix tests

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest')
-const app = require('../config/app')
 
 describe('Content-Type Middleware', () => {
+  let app
+
+  beforeEach(() => {
+    jest.resetModules()
+    app = require('../config/app')
+  })
+
   it('should return json content-type as default', async () => {
     app.get('/test', (req, res) => res.send(''))
 
@@ -11,13 +17,13 @@ describe('Content-Type Middleware', () => {
   })
 
   it('should return xml content-type if forced', async () => {
-    app.get('/test_2', (req, res) => {
+    app.get('/test', (req, res) => {
       res.type('xml')
       res.send('')
     })
 
     await request(app)
-      .get('/test_2')
+      .get('/test')
       .expect('content-type', /xml/)
   })
 })
